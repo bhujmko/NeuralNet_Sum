d7870792cb1a540b5b374cd70f3b3652440f80a8
bhujmko <arsikjou-jou@mail.ru>
2019-07-05 01:15:14 +0300

    to be continued...

--- NeuralNet_Sum/Neuron.h
+++ NeuralNet_Sum/Neuron.h
@@ -24,26 +24,7 @@ public:
 
 	double activate(double val) {
 		double res = 1 / (1 + exp(-1 * val));
-
-		if (res == 0) return 0;
-		else if (res > 0 && res <= 0.05) return 1;
-		else if (res > 0.05 && res <= 0.1) return 2;
-		else if (res > 0.1 && res <= 0.15) return 3;
-		else if (res > 0.15 && res <= 0.2) return 4;
-		else if (res > 0.2 && res <= 0.25) return 5;
-		else if (res > 0.25 && res <= 0.3) return 6;
-		else if (res > 0.3 && res <= 0.35) return 7;
-		else if (res > 0.35 && res <= 0.4) return 8;
-		else if (res > 0.4 && res <= 0.45) return 9;
-		else if (res > 0.45 && res <= 0.5) return 10;
-		else if (res > 0.5 && res <= 0.55) return 11;
-		else if (res > 0.55 && res <= 0.6) return 12;
-		else if (res > 0.6 && res <= 0.65) return 13;
-		else if (res > 0.65 && res <= 0.7) return 14;
-		else if (res > 0.7 && res <= 0.75) return 15;
-		else if (res > 0.75 && res <= 0.8) return 16;
-		else if (res > 0.8 && res <= 0.85) return 17;
-		else if (res > 0.85) return 18;
+		return res;
 	}
 
 private:
--- NeuralNet_Sum/main.cpp
+++ NeuralNet_Sum/main.cpp
@@ -1,8 +1,37 @@
 #include "Neuron.h"
 
+void MakeWeb(std::vector<std::vector<Neuron>>& neurons) {
+	neurons.emplace_back(std::vector<Neuron>(3));
+	neurons.emplace_back(std::vector<Neuron>(3));
+	
+	for (int i = 0; i < 3; i++) neurons[0][i] = Neuron(2); //first hiden layer initialization
+
+	neurons[1][0] = Neuron(2);
+	neurons[1][1] = Neuron(2);
+	neurons[1][0] = Neuron(1);
+
+	for (int i = 0; i < 2; i++) for (int j = 0; j < neurons[i].size(); j++) neurons[i][j].setWeights();
+
+	return;
+}
+
+void UseWeb(std::vector<std::vector<Neuron>>& neurons, int val1, int val2) {
+	
+	
+	return;
+}
+
 int main() {
+	std::vector<std::vector<Neuron>> neurons;
 
+	MakeWeb(neurons);
 
+	int val1;
+	int val2;
+
+	std::cin >> val1 >> val2;
+
+	UseWeb(neurons, val1, val2);
 
 	return 0;
 }
